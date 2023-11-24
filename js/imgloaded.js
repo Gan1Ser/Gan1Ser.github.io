fa35214b47d7665da5ab1d5ba195d0b0e7d9cb06
Gan1Ser <99937718+Gan1Ser@users.noreply.github.com>
2023-11-24 01:22:02 +0000

    🎉 INT COMMIT: Updated By Github Actions

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -71,10 +71,10 @@ class ProgressiveLoad {
   };
   
   const config = {
-    smallSrc: 'https://t.mwm.moe/pc', // 小图链接 尽可能配置小于100k的图片
-    largeSrc: 'https://t.mwm.moe/pc', // 大图链接 最终显示的图片
-    mobileSmallSrc: 'https://t.mwm.moe/mp', // 手机端小图链接 尽可能配置小于100k的图片
-    mobileLargeSrc: 'https://t.mwm.moe/mp', // 手机端大图链接 最终显示的图片
+    smallSrc: 'https://pic.gan1ser.top/post/15d794ed78d26de94f4de151ab26ac6.jpg', // 小图链接 尽可能配置小于100k的图片
+    largeSrc: 'https://pic.gan1ser.top/post/15d794ed78d26de94f4de151ab26ac6.jpg', // 大图链接 最终显示的图片
+    mobileSmallSrc: 'https://pic.gan1ser.top/post/15d794ed78d26de94f4de151ab26ac6.jpg', // 手机端小图链接 尽可能配置小于100k的图片
+    mobileLargeSrc: 'https://pic.gan1ser.top/post/15d794ed78d26de94f4de151ab26ac6.jpg', // 手机端大图链接 最终显示的图片
     enableRoutes: ['/'],
     };
 
