65ce81f5092289d60dcff918631ecf8776158b0a
Gan1Ser <99937718+Gan1Ser@users.noreply.github.com>
2024-09-01 04:47:04 +0000

    再试试 Updated By Github Actions

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -1,7 +1,100 @@
-// 首页头图加载优化
-/**
- * @description 实现medium的渐进加载背景的效果
- */
+// // 首页头图加载优化
+// /**
+//  * @description 实现medium的渐进加载背景的效果
+//  */
+// class ProgressiveLoad {
+//   constructor(smallSrc, largeSrc) {
+//     this.smallSrc = smallSrc;
+//     this.largeSrc = largeSrc;
+//     this.initTpl();
+//   }
+
+//   /**
+//    * @description 生成ui模板
+//    */
+//   initTpl() {
+//     this.container = document.createElement('div');
+//     this.smallStage = document.createElement('div');
+//     this.largeStage = document.createElement('div');
+//     this.smallImg = new Image();
+//     this.largeImg = new Image();
+//     this.container.className = 'pl-container';
+//     this.smallStage.className = 'pl-img pl-blur';
+//     this.largeStage.className = 'pl-img';
+//     this.container.appendChild(this.smallStage);
+//     this.container.appendChild(this.largeStage);
+//     this.smallImg.onload = this._onSmallLoaded.bind(this);
+//     this.largeImg.onload = this._onLargeLoaded.bind(this);
+//   }
+
+//   /**
+//    * @description 加载背景
+//    */
+//   ProgressiveLoad() {
+//     this.smallImg.src = this.smallSrc;
+//     this.largeImg.src = this.largeSrc;
+//   }
+
+//   /**
+//    * @description 大图加载完成
+//    */
+//   _onLargeLoaded() {
+//     this.largeStage.classList.add('pl-visible');
+//     this.largeStage.style.backgroundImage = `url('${this.largeSrc}')`;
+//   }
+
+//   /**
+//    * @description 小图加载完成
+//    */
+//   _onSmallLoaded() {
+//     this.smallStage.classList.add('pl-visible');
+//     this.smallStage.style.backgroundImage = `url('${this.smallSrc}')`;
+//   }
+// }
+
+// const executeLoad = (config, target) => {
+//   console.log('执行渐进背景替换');
+//   const isMobile = window.matchMedia('(max-width: 767px)').matches;
+//   const loader = new ProgressiveLoad(
+//     isMobile ? config.mobileSmallSrc : config.smallSrc,
+//     isMobile ? config.mobileLargeSrc : config.largeSrc
+//   );
+//   // 和背景图颜色保持一致，防止高斯模糊后差异较大
+//   if (target.children[0]) {
+//     target.insertBefore(loader.container, target.children[0]);
+//   }
+//   loader.ProgressiveLoad();
+// };
+
+// const config = {
+//   smallSrc: 'https://pic.gan1ser.top/post/14.webp/cover', // 小图链接 尽可能配置小于100k的图片
+//   largeSrc: 'https://pic.gan1ser.top/pic/1.webp', // 大图链接 最终显示的图片
+//   mobileSmallSrc: 'https://pic.gan1ser.top/pic/2.webp', // 手机端小图链接 尽可能配置小于100k的图片
+//   mobileLargeSrc: 'https://pic.gan1ser.top/pic/3.webp', // 手机端大图链接 最终显示的图片
+//   enableRoutes: ['/'],
+//   };
+
+// function initProgressiveLoad(config) {
+//   const target = document.getElementById('page-header');
+//   if (target && target.classList.contains('full_page')) {
+//     executeLoad(config, target);
+//   }
+// }
+
+// function onPJAXComplete(config) {
+//   const target = document.getElementById('page-header');
+//   if (target && target.classList.contains('full_page')) {
+//     initProgressiveLoad(config);
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   initProgressiveLoad(config);
+// });
+
+// document.addEventListener("pjax:complete", function() {
+//   onPJAXComplete(config);
+// });
 class ProgressiveLoad {
   constructor(smallSrc, largeSrc) {
     this.smallSrc = smallSrc;
@@ -30,7 +123,7 @@ class ProgressiveLoad {
   /**
    * @description 加载背景
    */
-  ProgressiveLoad() {
+  load() {  // 将方法名改为其他名称
     this.smallImg.src = this.smallSrc;
     this.largeImg.src = this.largeSrc;
   }
@@ -63,7 +156,7 @@ const executeLoad = (config, target) => {
   if (target.children[0]) {
     target.insertBefore(loader.container, target.children[0]);
   }
-  loader.ProgressiveLoad();
+  loader.load();  // 调用load方法
 };
 
 const config = {
